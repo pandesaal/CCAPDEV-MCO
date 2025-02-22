b262161cd71f48deeb8bd04024b556ec8f94371f
earairl <eara.irlandez@gmail.com>
2025-02-23 02:05:20 +0800

    added "delete profile" option; adjusted visual cues when editing profile

--- client/js/pages/profile.js
+++ client/js/pages/profile.js
@@ -14,13 +14,25 @@ function editProfile(e) {
     if (e.target.innerText === "Edit Profile") {
         document.getElementById("profilePic").classList.add("editable");
         document.getElementById("bio").classList.add("editable");
+        document.getElementById("deleteProfileBtn").classList.remove("hide");
         e.target.innerText = "Save Changes";
         bio.contentEditable = true;
+
+        // for blinking cursor during edit
+        bio.focus();
+        let range = document.createRange();
+        let selection = window.getSelection();
+        range.selectNodeContents(bio);
+        range.collapse(false);
+        selection.removeAllRanges();
+        selection.addRange(range);
+        
         profileImage.contentEditable = true;
     }
     else {
         document.getElementById("profilePic").classList.remove("editable");
         document.getElementById("bio").classList.remove("editable");
+        document.getElementById("deleteProfileBtn").classList.add("hide");
         e.target.innerText = "Edit Profile";
         bio.contentEditable = false;
         profileImage.contentEditable = false;

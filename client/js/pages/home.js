c08122a1b41382a128b200036adf013793502118
roms-jr <romeorasay11@gmail.com>
2025-02-22 10:34:09 +0800

    Delete client directory

--- client/js/pages/home.js
+++
@@ -1,7 +0,0 @@
-import { navInjector } from "../components/nav-injector.js";
-import { postInjector, posts } from "../components/post-injector.js";
-
-document.addEventListener('DOMContentLoaded', () => {
-    navInjector();
-    postInjector();
-});

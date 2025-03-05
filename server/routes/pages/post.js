0e38f832fc231fc1839f15df0e5bd61b2a25f7c2
pandesaal <asophiadd@gmail.com>
2025-03-05 14:13:48 +0800

    fixed post page not showing posts

--- server/routes/pages/post.js
+++ server/routes/pages/post.js
@@ -2,9 +2,11 @@ const getPostData = require('../../controllers/get-post');
 const page_renderer = require('../../utils/page-render');
 
 const post = async (req, res) => {
-    const post = await getPostData({postId: req.params.id, comments: true})
-    if (post[0])
-        page_renderer('post', req, res, {posts: post[0]});
+    const postObject = await getPostData({postId: req.params.id, comments: true})
+    const post = postObject.posts[0]
+    
+    if (post)
+        page_renderer('post', req, res, {posts: post});
     else
         res.status(404).render('404', {layout: false});
 }

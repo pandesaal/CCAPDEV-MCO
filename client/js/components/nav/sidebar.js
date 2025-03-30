e27ff8b42408a8a70f8af79762a8ddba3397b8a1
pandesaal <asophiadd@gmail.com>
2025-03-30 12:47:02 +0800

    bonus: dark mode

--- client/js/components/nav/sidebar.js
+++ client/js/components/nav/sidebar.js
@@ -15,3 +15,19 @@ sidebarClose.addEventListener("click", () => {
 sidebarOverlay.addEventListener("click", () => {
     sidebar.classList.toggle("hide")
 })
+
+const darkMode = document.getElementById('sidebar-dark-mode');
+
+if (localStorage.getItem('darkMode') === 'enabled') {
+    document.body.classList.add('dark-mode');
+}
+
+darkMode.addEventListener('click', () => {
+    document.body.classList.toggle('dark-mode');
+    
+    if (document.body.classList.contains('dark-mode')) {
+        localStorage.setItem('darkMode', 'enabled');
+    } else {
+        localStorage.setItem('darkMode', 'disabled');
+    }
+});
